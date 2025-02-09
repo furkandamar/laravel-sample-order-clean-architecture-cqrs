72b9ebd721399f37fbd3cf54505b7c6249e80c8f
fukoapps <f.damar@hotmail.com>
2025-02-09 17:04:17 +0300

    refactor readme
    add postman collection
    refactor api route

--- src/routes/api.php
+++ src/routes/api.php
@@ -11,11 +11,12 @@ Route::middleware([LogRequestResponse::class])->group(function () {
 
     Route::post("/login", [AuthController::class, "login"]);
 
+    Route::get("/products", [ProductController::class, "getProducts"]);
+    Route::get("/categories", [ProductController::class, "getCategories"]);
+
     Route::middleware([JwtMiddleware::class])->group(function () {
         Route::get("/me", [AuthController::class, "me"]);
 
-        Route::get("/products", [ProductController::class, "getProducts"]);
-        Route::get("/categories", [ProductController::class, "getCategories"]);
 
         Route::get("/orders", [OrderController::class, "getOrders"]);
         Route::post("/orders", [OrderController::class, "createOrder"]);
